36a552c14922a84315012a9e547aba758b45eb7b
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-07-03 22:28:38 +0530

    fix: 🐛 broken default gateway url in `useImgUris`

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, HTMLAttributes, useEffect, useState } from 'react';
 
-const DEFAULT_IPFS_GATEWAY_URL = 'https://ipfs.infura.io/ipfs/';
+const DEFAULT_IPFS_GATEWAY_URL = 'https://ipfs.infura.io/ipfs';
 
 const cleanUpHash = (hash: string) => {
   // remove the leading 'ipfs://' if it exists
@@ -28,7 +28,7 @@ export const getImgUriFromTokenUriHash = async (
   ipfsGatewayUrl = DEFAULT_IPFS_GATEWAY_URL
 ) => {
   const ipfsHash = cleanUpHash(tokenUriHash);
-  const ipfsUrl = `${ipfsGatewayUrl}${ipfsHash}`;
+  const ipfsUrl = `${ipfsGatewayUrl}/${ipfsHash}`;
   const res = await (await fetch(ipfsUrl)).json();
   return res.image as string;
 };
