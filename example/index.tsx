753114743173fce8f12ed83ae93292022d9caf31
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-07-03 20:40:11 +0530

    feat: 🎸 docs and support for alt hash format

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { IpfsImage } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <IpfsImage hash="Qme8SriYgGNoXQzG1qYYZKThv3QTBf7pMJwUpu3gqaqQRH" />
     </div>
   );
 };
