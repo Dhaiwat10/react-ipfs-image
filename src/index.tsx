64a33961ad0964e6b7ac2930eecbe549201b1cca
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-07-03 22:21:54 +0530

    feat: 🎸 add `useImgUris` and remove GH workflows

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import React, { FC, HTMLAttributes } from 'react';
+import React, { FC, HTMLAttributes, useEffect, useState } from 'react';
+
+const DEFAULT_IPFS_GATEWAY_URL = 'https://ipfs.infura.io/ipfs';
 
 const cleanUpHash = (hash: string) => {
   // remove the leading 'ipfs://' if it exists
@@ -15,8 +17,34 @@ export interface IIpfsImageProps extends HTMLAttributes<HTMLImageElement> {
 
 export const IpfsImage: FC<IIpfsImageProps> = ({
   hash,
-  gatewayUrl = 'https://ipfs.infura.io/ipfs',
+  gatewayUrl = DEFAULT_IPFS_GATEWAY_URL,
   ...props
 }) => {
   return <img src={`${gatewayUrl}/${cleanUpHash(hash)}`} {...props} />;
+};
+
+export const getImgUriFromTokenUriHash = async (
+  tokenUriHash: string,
+  ipfsGatewayUrl = DEFAULT_IPFS_GATEWAY_URL
+) => {
+  const ipfsHash = cleanUpHash(tokenUriHash);
+  const ipfsUrl = `${ipfsGatewayUrl}${ipfsHash}`;
+  const res = await (await fetch(ipfsUrl)).json();
+  return res.image as string;
+};
+
+export const useImgUris = (tokenUriHashes: string[]) => {
+  const [imgURIs, setImgURIs] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchImgUris = async () => {
+      const imgUris = await Promise.all(
+        tokenUriHashes.map(uriHash => getImgUriFromTokenUriHash(uriHash))
+      );
+      setImgURIs(imgUris);
+    };
+    fetchImgUris();
+  }, [tokenUriHashes]);
+
+  return imgURIs;
 };
